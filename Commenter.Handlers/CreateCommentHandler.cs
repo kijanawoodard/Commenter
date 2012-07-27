51b2b23d950baee584675f404f454318301a95d4
Kijana Woodard <github@wyldeye.com>
2012-07-27 18:41:06 -0500

    saving in raven

--- Commenter.Handlers/CreateCommentHandler.cs
+++ Commenter.Handlers/CreateCommentHandler.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Commenter.Documents;
 using Commenter.Messages.Commands;
 using NServiceBus;
+using Raven.Client;
 
 namespace Commenter.Handlers
 {
     class CreateCommentHandler : IHandleMessages<CreateComment>
     {
+        private readonly IDocumentSession _session;
+
+        public CreateCommentHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
+
         public void Handle(CreateComment message)
         {
-            Console.WriteLine("Hello");
+            var comment =
+                new Comment
+                    {
+                        PostId = message.PostId,
+                        Who = message.Who,
+                        When = DateTime.UtcNow,
+                        What = message.What
+                    };
+
+            _session.Store(comment);
+            
+            Console.WriteLine("Comment Saved");
         }
     }
 }
--- Commenter.Handlers/EndpointConfig.cs
+++ Commenter.Handlers/EndpointConfig.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NServiceBus;
+using NServiceBus.Persistence.Raven;
+using Raven.Client;
+using Raven.Client.Document;
+using NServiceBus.UnitOfWork;
+using StructureMap;
 
 namespace Commenter.Handlers
 {
@@ -10,12 +15,68 @@ namespace Commenter.Handlers
     {
         public void Init()
         {
-            Configure.With()
+            Configure
+                .With()
                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Events"))
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Commands"))
                 .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages"))
-                .DefaultBuilder()
+                .StructureMapBuilder()
                 .JsonSerializer();
+        }
+    }
+
+    public class SetupContainer : IWantCustomInitialization
+    {
+        public void Init()
+        {
+            const string database = "comments";
+            var store = new DocumentStore
+            {
+                Url = "http://localhost:8080",
+                DefaultDatabase = database
+            };
+            store.Initialize();
+
+            ObjectFactory.Configure(c =>
+            {
+                c.For<IDocumentStore>()
+                    .Singleton()
+                    .Use(store);
+
+                c.For<IDocumentSession>()
+                    .Use(ctx => ctx.GetInstance<IDocumentStore>().OpenSession());
+
+                c.For<IManageUnitsOfWork>()
+                    .Use<RavenUnitOfWork>();
+            });
+        }
+    }
+
+    public class StartStop : IWantToRunAtStartup
+    {
+        public void Run()
+        {
+//            Console.TreatControlCAsInput = false;
+//            Console.CancelKeyPress += OnCancelKeyPress;
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        internal void OnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+//            Console.WriteLine("hello");
+//            var store = ObjectFactory.GetInstance<IDocumentStore>();
+//            if (store != null)
+//            {
+//                Console.WriteLine("disposing raven store");
+//                store.Dispose();
+//            }
+//
+//            e.Cancel = true;
+//           Stop();
         }
     }
 }
